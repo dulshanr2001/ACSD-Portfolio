d4ef015ed343824b68b66417404e7b331a3a4ffd
Ravishka Dulshan <137523505+dulshanr2001@users.noreply.github.com>
2024-11-05 02:49:32 +0530

    I finished js file

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const textElement = document.getElementById('portfolio');
+    const text = "I'm Ravishka Dulshan"; 
+    let index = 0;
+
+    function type() {
+        if (index < text.length) {
+            textElement.textContent += text.charAt(index);
+            index++;
+            setTimeout(type, 100); 
+        }
+    }
+
+    type(); 
+});
 
